4eb8e7f73861fed7b30fe4749d904ab780553c1f
Kovács Bence <78758122+neuroben@users.noreply.github.com>
2021-08-27 18:52:06 +0200

    Első feladat titkosítás
    Első feladat első része kész. Az emailban kapott példában a kulcsnál hiányzik a H betű. Ezt pótoltam valószínüleg azért más a kapott titkosítás. A feladatot angolul oldottam meg.

--- elso_feladat.py
+++ elso_feladat.py
@@ -1,10 +1,10 @@
 #   - a rejtjelezett üzenet n. karaktere az üzenet n. karakterének kódja + kulcs n. karakterének kódja
 # üzenet: "hello world"
-# kulcs:  "abcdefgijkl" <---  hiányzik a H-betű ---> jó "abcdefghijkl"
+# kulcs:  "abcdefgijkl" <---  hiányzik a H-betű ---> jó "abcdefghijk"
 # rejtjelezett üzenet: "hfnosauzun"
 # 7 4 11 11 14 26 22 14 17 11 3
 # 0 1 2  3  4  5  6  7  8  9  10
-# 7 5 13 14 18 4  1  6  25 21 13
+# 7 5 13 14 18 4  1  21   25 20 13
 abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
 
 line_in = input("Message: ")
@@ -16,20 +16,41 @@ key_in_code = []
 encrypted_message_code = []
 encrypted_message = []
 
-
-
 def txt_to_code(txt):
     code = []
     for letter in txt:
         code.append(abc.index(letter))
-        print(letter,abc.index(letter))
     return code
 
 line_in_code = txt_to_code(line_in)
 key_in_code = txt_to_code(key_in)
 
-print(line_in_code,len(line_in_code))
-print(key_in_code,len(key_in_code))
+# reviewing the key length relative to the message
+
+while len(line_in_code) > len(key_in_code):
+    print("The key can not be shorter than the message.")
+    key_in_code = txt_to_code(input("Please give a key which length is equal to the message: "))
+
+for i,v in enumerate(line_in_code):
+    if v + key_in_code[i] < 26:
+        encrypted_message_code.append(v + key_in_code[i])
+    else:
+        encrypted_message_code.append((v + key_in_code[i]) % 27)
+
+
+def code_to_txt(code):
+    txt = []
+    for number in code:
+        txt.append(abc[number])
+    return txt
+
+encrypted_message = ''.join(code_to_txt(encrypted_message_code))
+
+print('Encrypted message:',encrypted_message)
+
+
+
+
 
 
 
